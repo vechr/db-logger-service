a66a7246582c074ae5fe87e775021c21562256eb
zulfikar4568 <isnaen70@gmail.com>
2023-08-13 20:46:58 +0700

    fix: / path health check

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Get, Version, VERSION_NEUTRAL } from '@nestjs/common';
-import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Version,
+  VERSION_NEUTRAL,
+} from '@nestjs/common';
+import {
+  HealthCheckService,
+  HealthCheck,
+  HealthCheckResult,
+} from '@nestjs/terminus';
 
-@Controller('health')
+@Controller()
 export default class HealthController {
   constructor(private health: HealthCheckService) {}
 
   @Version(VERSION_NEUTRAL)
+  @Get('health')
+  @HealthCheck()
+  @HttpCode(HttpStatus.OK)
+  check() {
+    return this.healthCheck();
+  }
+
+  @Version(VERSION_NEUTRAL)
   @Get()
   @HealthCheck()
-  check() {
+  @HttpCode(HttpStatus.OK)
+  check2() {
+    return this.healthCheck();
+  }
+
+  private async healthCheck(): Promise<HealthCheckResult> {
     return this.health.check([]);
   }
 }
